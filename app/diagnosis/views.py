15df94334b6106cb3ea6afd1f0dbd2cba3ecb252
nanseong <nanseong@gmail.com>
2026-06-18 22:46:41 +0900

    add pages app,  add web page html & css

--- app/diagnosis/views.py
+++ app/diagnosis/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def diagnosis_intro(request):
+    return render(request, "diagnosis/intro.html")
+
+
+def diagnosis_exam(request):
+    return render(request, "diagnosis/exam.html")
