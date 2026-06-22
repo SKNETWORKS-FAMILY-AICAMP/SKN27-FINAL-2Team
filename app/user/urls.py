feb12c15bf63bf8738f4050dbbc569079e26745c
nanseong <nanseong>
2026-06-22 20:24:36 +0900

    add wrong_rate page, solved_problem page

--- app/user/urls.py
+++ app/user/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path("check-nickname/", views.check_nickname, name="check_nickname"),
     path("mypage/", views.mypage, name="mypage"),
     path("profile/edit/", views.profile_edit, name="profile_edit"),
+    path("solved-problems/", views.solved_problems, name="solved_problems"),
     path("wrong-note/", views.wrong_note, name="wrong_note"),
+    path("wrong-rate-detail/", views.wrong_rate_detail, name="wrong_rate_detail"),
 ]
