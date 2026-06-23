340776f0526d7f13fcd125073822728311d4565f
m2k-dcyh13 <219132320+m2k-dcyh13@users.noreply.github.com>
2026-06-23 17:22:47 +0900

    정리

--- app/analytics/service/mypage.py
+++ app/analytics/service/mypage.py
@@ -1,11 +1,12 @@
-# mypage에서 분석 대시보드, 학습계획 정보 조회 서비스
+from analytics.models import NoteMypage
+from analytics.service.analystic import analytics_summary
+from analytics.service.studyplan import get_study_plan_info
 
-from app.analytics.models import StudyPlanMypage, NoteMypage
 
 def get_mypage_info(user_id):
-    # 학습계획 정보 조회
-    study_plan = StudyPlanMypage.objects.get(user_id=user_id)
-    # 오답노트 정보 조회
-    note = NoteMypage.objects.get(user_id=user_id)
-    # 분석 대시보드 정보 조회
-    
+    notes = NoteMypage.objects.filter(user_id=user_id).order_by("-modified_at")
+    return {
+        "studyPlans": get_study_plan_info(user_id),
+        "notes": list(notes),
+        "analytics": analytics_summary(user_id),
+    }
