be7e587aab7e686de1c21c70b56401a658d92794
nanseong <nanseong>
2026-06-26 11:19:38 +0900

    add favicon, add logo, change loading page

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.png', permanent=True)),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('chatbot/', include('chatbot.urls')),
